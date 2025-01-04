1473468e4e4104163208aae4c5c873bf7f4e2549
robert-1e <rellard19@cms.tela.org.uk>
2025-01-04 15:52:36 +0000

    Fixed some things

--- server.js
+++ server.js
@@ -22,10 +22,14 @@ Deno.serve({
         } else {
             const URLPath = new URL(request.url).pathname;
 
-            if (URLPath === "signup") {
-            } else if (URLPath === "login") {
+            let filePath;
+
+            if (URLPath === "/signup") {
+                filePath = "./web/accounts/signup.html";
+            } else if (URLPath === "/login") {
+                filePath = "./web/accounts/login.html";
             } else {
-                let filePath = `./web${URLPath.match(/^[^&]*/)}`; // Thank you to https://regexr.com and https://chatgpt.com for this regex
+                filePath = `./web${URLPath.match(/^[^&]*/)}`; // Thank you to https://regexr.com and https://chatgpt.com for this regex
 
                 if (!filePath.endsWith("/") && !filePath.endsWith(".html")) {
                     filePath += "/";
@@ -34,19 +38,19 @@ Deno.serve({
                 if (!filePath.endsWith(".html")) {
                     filePath += "index.html";
                 }
-
-                let file;
-
-                try {
-                    file = await Deno.open(filePath, { read: true });
-                } catch (error) {
-                    console.log(error);
-
-                    file = await Deno.open("./web/404.html", { read: true });
-                }
-
-                return new Response(file.readable);
             }
+
+            let file;
+
+            try {
+                file = await Deno.open(filePath, { read: true });
+            } catch (error) {
+                console.log(error);
+
+                file = await Deno.open("./web/404.html", { read: true });
+            }
+
+            return new Response(file.readable);
         }
     },
 });
