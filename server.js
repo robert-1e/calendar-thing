5387727f1f0acce6136301386e1228f20d5f7f63
robert-1e <rellard19@cms.tela.org.uk>
2025-01-04 14:57:05 +0000

    fixed spelling err

--- server.js
+++ server.js
@@ -23,29 +23,30 @@ Deno.serve({
             const URLPath = new URL(request.url).pathname;
 
             if (URLPath === "signup") {
+            } else if (URLPath === "login") {
+            } else {
+                let filePath = `./web${URLPath.match(/^[^&]*/)}`; // Thank you to https://regexr.com and https://chatgpt.com for this regex
+
+                if (!filePath.endsWith("/") && !filePath.endsWith(".html")) {
+                    filePath += "/";
+                }
+
+                if (!filePath.endsWith(".html")) {
+                    filePath += "index.html";
+                }
+
+                let file;
+
+                try {
+                    file = await Deno.open(filePath, { read: true });
+                } catch (error) {
+                    console.log(error);
+
+                    file = await Deno.open("./web/404.html", { read: true });
+                }
+
+                return new Response(file.readable);
             }
-
-            let filePath = `./web${URLPath.match(/^[^&]*/)}`; // Thank you to https://regexr.com and https://chatgpt.com for this regex
-
-            if (!filePath.endsWith("/") && !filePath.endsWith(".html")) {
-                filePath += "/";
-            }
-
-            if (!path.endsWith(".html")) {
-                filePath += "index.html";
-            }
-
-            let file;
-
-            try {
-                file = await Deno.open(filePath, { read: true });
-            } catch (error) {
-                console.log(error);
-
-                file = await Deno.open("./web/404.html", { read: true });
-            }
-
-            return new Response(file.readable);
         }
     },
 });
