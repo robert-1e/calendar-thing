3818ae16f15b8017b571a415702f5e47021a5ce5
robert-1e <rellard19@cms.tela.org.uk>
2025-01-07 12:47:16 +0000

    commit #68

--- web/accounts/signup.js
+++ web/accounts/signup.js
@@ -8,21 +8,21 @@ const errorMsgP = document.getElementById("error-messages-tmp");
 
 function validateForm() {
     let errors = [];
-    if (!nameInp.textContent) {
+    if (!nameInp.value) {
         errors.push("Enter a username");
-    } else if (nameInp.textContent.length < 5) {
+    } else if (nameInp.value.length < 5) {
         errors.push("Username must be at least 5 characters long");
-    } else if (18 < nameInp.textContent.length) {
+    } else if (18 < nameInp.value.length) {
         errors.push("Username must be at most 18 characters long");
-    } else if (nameInp.textContent.test(/[^a-zA-Z0-9_]/)) {
+    } else if (nameInp.value.test(/[^a-zA-Z0-9_]/)) {
         errors.push(
             "Username cannot contain any fancy shmancy characters and that, stick to letters and numbers and underscores"
         );
     }
 
-    if (passInp.textContent.length < 8) {
+    if (passInp.value.length < 8) {
         errors.push("Password must be at least 8 characters long");
-    } else if (20 < passInp.textContent.length) {
+    } else if (20 < passInp.value.length) {
         errors.push("Password must be at most 20 characters long");
     }
 
