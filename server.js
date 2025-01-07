6e9d1888754b3c6c6a195781d4275d4111ed6720
robert-1e <rellard19@cms.tela.org.uk>
2025-01-07 12:08:41 +0000

    Hopefully fixed a bug concerning filetypes other than .html being sent to the client on request

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ Deno.serve({
             } else {
                 filePath = `./web${URLPath.match(/^[^&]*/)}`; // Thank you to https://regexr.com and https://chatgpt.com for this regex
 
-                if (!filePath.endsWith("/") && !filePath.endsWith(".html")) {
+                if (!filePath.endsWith("/") && !filePath.test(/\.[^\/\.]+$/)) {
                     filePath += "/";
                 }
 
-                if (!filePath.endsWith(".html")) {
+                if (!filePath.test(/\.[^\/\.]+$/)) {
                     filePath += "index.html";
                 }
             }
