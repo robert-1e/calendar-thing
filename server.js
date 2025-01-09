dac90022e71f341093a854fadba8b3cc10c3bd59
robert-1e <rellard19@cms.tela.org.uk>
2025-01-09 23:46:16 +0000

    made some changes i think (hope it dont break)

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-await Deno.openKv();
+const kv = await Deno.openKv();
 
 Deno.serve({
     port: 80,
     handler: async (request) => {
-        if (request.headers.get("upgrade") === "websocket") {
-            // Currently just code stolen from the internets
-            // TODO: make it like actually do what it is supposed to do
+        try {
+            if (request.headers.get("upgrade") === "websocket") {
+                // Currently just code stolen from the internets
+                // TODO: make it like actually do what it is supposed to do
 
-            const { socket, response } = Deno.upgradeWebSocket(request);
+                const { socket, response } = Deno.upgradeWebSocket(request);
 
-            socket.onopen = () => {
-                console.log("CONNECTED");
-            };
-            socket.onmessage = (event) => {
-                console.log(`RECEIVED: ${event.data}`);
-                socket.send("pong");
-            };
-            socket.onclose = () => {
-                console.log("DISCONNECTED");
-            };
-            socket.onerror = (error) => {
-                console.error("ERROR:", error);
-            };
+                socket.onopen = () => {
+                    console.log("CONNECTED");
+                };
+                socket.onmessage = (event) => {
+                    console.log(`RECEIVED: ${event.data}`);
+                    socket.send("pong");
+                };
+                socket.onclose = () => {
+                    console.log("DISCONNECTED");
+                };
+                socket.onerror = (error) => {
+                    console.error("ERROR:", error);
+                };
 
-            return response;
-        } else if (request.method === "POST") {
-            console.log(request);
+                return response;
+            } else if (request.method === "POST") {
+                console.log(request);
 
-            try {
-                switch (new URL(request.url).pathname) {
-                    case "/signup/new-account":
-                        try {
+                try {
+                    switch (new URL(request.url).pathname) {
+                        case "/signup/new-account":
                             let accInfo = JSON.parse(await request.text());
 
-                            console.log("post req worked");
+                            console.log(accInfo);
 
                             // Validating info server-side (as well as client side)
                             if (
@@ -43,57 +43,69 @@ Deno.serve({
                                 20 < accInfo.password.length
                             ) {
                                 // Invalid info (deal with it somehow)
+                                console.log(
+                                    "FIX ME!1!!!1\n(if this is still WIP)\n[FOR DEBUGGING PURPOSES]"
+                                );
+
+                                return new Response("invalid account creation data", {
+                                    status: 400,
+                                    headers: { "content-type": "text/html" },
+                                });
                             }
-                        } catch (error) {
-                            console.log("oh noes we got an error1?!/!1");
-                        }
 
-                        break;
+                            kv.get([accounts]);
 
-                    default:
-                        console.log("Unknown POST request\n", request);
+                            break;
 
-                        return new Response(/* TODO: write this bit */);
+                        default:
+                            console.log("Unknown POST request\n", request);
+
+                            return new Response(/* TODO: write this bit */);
+                    }
+                } catch (error) {
+                    console.log("Invalid POST request");
                 }
-            } catch (error) {
-                console.log("Invalid POST request");
-            }
 
-            return new Response();
-        } else {
-            const URLPath = new URL(request.url).pathname;
-
-            let filePath;
-
-            if (/^\/(signup|login)\/*$/.test(URLPath)) {
-                filePath = `./web/accounts/${URLPath.match(/signup|login/i)}.html`;
-            } else if (/^\/(signup|login)\.js$/.test(URLPath)) {
-                filePath = "./web/accounts" + URLPath;
+                return new Response();
             } else {
-                filePath = `./web${URLPath.match(/^[^&]*/)}`;
+                const URLPath = new URL(request.url).pathname;
 
-                if (!filePath.endsWith("/") && !/\.[^\/\.]+$/.test(filePath)) {
-                    filePath += "/";
+                let filePath;
+
+                if (/^\/(signup|login)\/*$/.test(URLPath)) {
+                    filePath = `./web/accounts/${URLPath.match(/signup|login/i)}.html`;
+                } else if (/^\/(signup|login)\.js$/.test(URLPath)) {
+                    filePath = "./web/accounts" + URLPath;
+                } else {
+                    filePath = `./web${URLPath.match(/^[^&]*/)}`;
+
+                    if (!filePath.endsWith("/") && !/\.[^\/\.]+$/.test(filePath)) {
+                        filePath += "/";
+                    }
+
+                    if (!/\.[^\/\.]+$/.test(filePath)) {
+                        filePath += "index.html";
+                    }
                 }
 
-                if (!/\.[^\/\.]+$/.test(filePath)) {
-                    filePath += "index.html";
+                let file;
+
+                try {
+                    file = await Deno.open(filePath, { read: true });
+                } catch (error) {
+                    file = await Deno.open("./web/404.html", { read: true });
+
+                    if (error.name !== "NotFound") {
+                        console.log(error);
+                    }
                 }
+
+                return new Response(file.readable);
             }
-
-            let file;
-
-            try {
-                file = await Deno.open(filePath, { read: true });
-            } catch (error) {
-                file = await Deno.open("./web/404.html", { read: true });
-
-                if (error.name !== "NotFound") {
-                    console.log(error);
-                }
-            }
-
-            return new Response(file.readable);
+        } catch (_) {
+            new Response({
+                status: 500,
+            });
         }
     },
 });
